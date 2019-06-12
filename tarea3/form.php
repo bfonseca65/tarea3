af060fd94520424bf10314a7a99c941d4d2dd18f
Bryan Fonseca F <45154384+bfonseca65@users.noreply.github.com>
2019-06-12 12:06:17 -0600

    Add files via upload

--- tarea3/form.php
+++ tarea3/form.php
@@ -1,9 +1,28 @@
 <?php
 
-    $nombre = $_GET['nombre'];
-    $contrasena = $_GET['contrasena'];
-    $email = $_GET['email'];
-    $fecha = $_GET['date'];
+    if(isset($_GET['submit'])){
+        
+        $nombre = $_GET['nombre'];
+        $contrasena = $_GET['contrasena'];
+        $email = $_GET['email'];
+        $fecha = $_GET['date'];
+}
 
     echo $nombre;
+
+    if(isset($_GET['submit'])){
+        if(empty($nombre)){
+            echo"<p class='error'>*Nombre vacio<p/>";
+        }
+        if(empty($contrasena)){
+            echo"<p class='error'>*Contraseña vacia<p/>";
+        }
+        if(empty($email)){
+            echo"<p class='error'>*Email vacio<p/>";
+        }
+        if(empty($fecha)){
+            echo"<p class='error'>*Fecha vacia<p/>";
+        }
+    }
+
 ?>
--- tarea3/validarFormulario.js
+++ tarea3/validarFormulario.js
@@ -1,20 +1,28 @@
 function validar() {
     
-    var nombre, contrasena, email, fecha, expreg;
+    var nombre, contrasena, email, fecha, expRegEmail, expRegNombre ;
 
     nombre = document.getElementById("nombre").value;
     contrasena = document.getElementById("contrasena").value; 
     email = document.getElementById("email").value;
     fecha = document.getElementById("date").value;
     
-    expreg = new RegExp("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+    expRegEmail = new RegExp("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
        
+    expRegNombre = new RegExp("^[a-z]+\.[a-z]+$");
+  
     
-    if(!expreg.test(email)){
+    if(!expRegNombre.test(nombre)){
+        
+        alert("Nombre incorrecto");
+        return false;
+    }
+    
+    if(!expRegEmail.test(email)){
         
         alert("Email incorrecto");
         return false;
     }
-    
+
         
 }
